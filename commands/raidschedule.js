b6152158a84b340bb301b2196588ff9d033d8d79
SausageManZ <42648988+SausageManZ@users.noreply.github.com>
2018-09-25 15:19:36 +0200

    Update raidschedule.js

--- commands/raidschedule.js
+++ commands/raidschedule.js
@@ -2,9 +2,7 @@ const Discord = require('discord.js');
 
 module.exports.run = async(bot, message, args) => {
     var interval = setInterval (function () {
-        // use the message's channel (TextChannel) to send a new message
-        message.channel.send("123")
-        .catch(console.error); // add error handling here
+      
     }, 1 * 1000); 
     message.channel.send("Hello world!")
 }
