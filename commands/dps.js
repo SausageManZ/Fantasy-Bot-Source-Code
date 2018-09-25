96e3e1d32216c64199a4e74be3778d58652790c5
SausageManZ <42648988+SausageManZ@users.noreply.github.com>
2018-09-25 23:34:06 +0200

    Update dps.js

--- commands/dps.js
+++ commands/dps.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports.run = async(bot, message, args) => {
-      message.author.reply("Prototype command called!");
+      message.author.send("Prototype command called!");
 }
 
 module.exports.help = {
